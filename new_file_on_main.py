6ed75b7590ac72bbb3058cc3574850212c16b977
CarlosFPG <carlos.patinogarcia9447@upse.edu.ec>
2026-02-21 17:00:47 -0500

    new file v2 feature1

--- new_file_on_main.py
+++ new_file_on_main.py
@@ -1,4 +1,4 @@
 # Ha aparecido un nuevo directorio en la rama main, mientras se estaba
 # trabajando independientemente con la rama feature1
 
-print("Hola desde la rama main, se ha creado un nuevo archivo llamado new_file_on_main.py")
+print("Hola desde la rama feature1, he modificado new_file_on_main.py")
